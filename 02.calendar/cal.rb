75e8dab495587c8e86a240b5ecde6d5bd4c5afd5
Massimo <dayoujianghong@gmail.com>
2021-04-28 19:35:08 +0900

    Edit cal.rb file -> change date to target_date

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,22 +5,22 @@ require 'optparse'
 
 opt = OptionParser.new
 
-date = Date.today
+target_date = Date.today
 
 opt.on('-y', '--year VALUE', "year value") do |y|
-  date = Date.new(y.to_i, date.month)
+  target_date = Date.new(y.to_i, target_date.month)
 end
 
 opt.on('-m', '--month VALUE', "month value") do |m|
-  date = Date.new(date.year, m.to_i)
+  target_date = Date.new(target_date.year, m.to_i)
 end
 
 opt.parse!(ARGV)
 
-first_date = Date.new(date.year, date.month, 1)
-last_date = Date.new(date.year, date.month, -1)
+first_date = Date.new(target_date.year, target_date.month, 1)
+last_date = Date.new(target_date.year, target_date.month, -1)
 
-puts "#{date.month}月 #{date.year}年".center(20)
+puts "#{target_date.month}月 #{target_date.year}年".center(20)
 
 puts "日 月 火 水 木 金 土"
 
