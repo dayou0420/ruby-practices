ef27c617cfe93934b2d0f33bbfa1dd81467ebe57
Massimo <dayoujianghong@gmail.com>
2021-05-06 11:44:17 +0900

    Add bowling.rb file -> create bowling project

---
+++ 04.bowling/bowling.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+score = ARGV[0]
+
+scores = score.split(',')
+
+shots = []
+scores.each do |s|
+  if s == 'X'
+    shots.push(10)
+    shots.push(0)
+  else
+    shots.push(s.to_i)
+  end
+end
+
+frames = []
+shots.each_slice(2) do |s|
+  frames.push(s)
+end
+
+p frames
+
+point = 0
+shots.each do |x|
+  point += x
+end
+
+frames.each_with_index do |frame, i|
+  if frames[i][0] == 10 && frames[i + 1][0] == 10
+    point += frames[i + 1][0] + frames[i + 2][0]
+  elsif frames[i][0] == 10 && frames[i + 1][0] < 10
+    point += frames[i + 1][0] + frames[i + 1][1]
+  elsif frame.sum == 10 && frames[i][0] < 10 && i < 9
+    point += frames[i + 1][0]
+  end
+  break if i == 8
+end
+
+puts point
+
