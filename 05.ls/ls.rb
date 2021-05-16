77cab7a244c7e80845b84ea127880b2bea494766
Massimo <dayoujianghong@gmail.com>
2021-05-17 08:47:14 +0900

    Edit ls.rb file -> put the process to get the maximum value out of each

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -64,28 +64,34 @@ elsif params['l']
     file.join
   end
 
+  hard_link = files.compact.map { |f| File.stat(f.strip).nlink.to_s }
+  hard_link_length = hard_link.max_by(&:length).length
+
+  file_byte = files.compact.map { |f| File.stat(f.strip).size.to_s }
+  file_byte_length = file_byte.max_by(&:length).length
+
+  file_time_stamp_month = files.compact.map { |f| File.stat(f.strip).mtime.month.to_s }
+  file_time_stamp_month_length = file_time_stamp_month.max_by(&:length).length
+
+  file_time_stamp_day = files.compact.map { |f| File.stat(f.strip).mtime.day.to_s }
+  file_time_stamp_day_length = file_time_stamp_day.max_by(&:length).length
+
+  file_time_stamp_time = files.compact.map { |f| File.stat(f.strip).mtime.strftime('%H:%M') }
+  file_time_stamp_time_length = file_time_stamp_time.max_by(&:length).length
+
   files.compact.each do |file|
-    hard_link = files.compact.map { |f| File.stat(f.strip).nlink.to_s }
-    hard_link_length = hard_link.max_by(&:length).length
     hard_link_output = File.stat(file.strip).nlink.to_s.rjust(hard_link_length)
 
     file_owner = Etc.getpwuid(File.stat(file.strip).uid).name
+
     file_group = Etc.getgrgid(File.stat(file.strip).gid).name
 
-    file_byte = files.compact.map { |f| File.stat(f.strip).size.to_s }
-    file_byte_length = file_byte.max_by(&:length).length
     file_byte_output = File.stat(file.strip).size.to_s.rjust(file_byte_length)
 
-    file_time_stamp_month = files.compact.map { |f| File.stat(f.strip).mtime.month.to_s }
-    file_time_stamp_month_length = file_time_stamp_month.max_by(&:length).length
     file_time_stamp_month_output = File.stat(file.strip).mtime.month.to_s.rjust(file_time_stamp_month_length)
 
-    file_time_stamp_day = files.compact.map { |f| File.stat(f.strip).mtime.day.to_s }
-    file_time_stamp_day_length = file_time_stamp_day.max_by(&:length).length
     file_time_stamp_day_output = File.stat(file.strip).mtime.day.to_s.rjust(file_time_stamp_day_length)
 
-    file_time_stamp_time = files.compact.map { |f| File.stat(f.strip).mtime.strftime('%H:%M') }
-    file_time_stamp_time_length = file_time_stamp_time.max_by(&:length).length
     file_time_stamp_time_output = File.stat(file.strip).mtime.strftime('%H:%M').rjust(file_time_stamp_time_length)
 
     puts "#{file_type(file)}#{file_mode(file)} #{hard_link_output} #{file_owner} #{file_group} #{file_byte_output}"\
