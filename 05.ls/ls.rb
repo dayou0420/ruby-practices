c9f308d67dbec15a137455db9a3c446e93b7926d
Massimo <dayoujianghong@gmail.com>
2021-05-18 11:19:23 +0900

    Edit ls.rb file -> change variable name

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -20,7 +20,7 @@ def ls_command(target_files)
   result.each { |row| puts row.join(' ') }
 end
 
-def file_type(file)
+def symbol_type(file)
   file = File.stat(file.strip).ftype
   file_type = {
     'directory' => 'd',
@@ -29,7 +29,7 @@ def file_type(file)
   file_type[file]
 end
 
-def file_mode(file)
+def permissions(file)
   mode = File.stat(file.strip).mode.to_s(8)
   mode = mode[-3, 3].chars.map { |m| m }
 
@@ -49,42 +49,42 @@ def file_mode(file)
   file.join
 end
 
-def file_hard_link(file)
+def hard_link(file)
   hard_link = @target_files.map { |f| File.stat(f.strip).nlink.to_s }
   hard_link_length = hard_link.max_by(&:length).length
   File.stat(file.strip).nlink.to_s.rjust(hard_link_length)
 end
 
-def file_byte(file)
-  file_byte = @target_files.compact.map { |f| File.stat(f.strip).size.to_s }
-  file_byte_length = file_byte.max_by(&:length).length
-  File.stat(file.strip).size.to_s.rjust(file_byte_length)
+def byte_size(file)
+  byte = @target_files.compact.map { |f| File.stat(f.strip).size.to_s }
+  byte_length = byte.max_by(&:length).length
+  File.stat(file.strip).size.to_s.rjust(byte_length)
 end
 
-def file_time_stamp_month(file)
-  file_time_stamp_month = @target_files.compact.map { |f| File.stat(f.strip).mtime.month.to_s }
-  file_time_stamp_month_length = file_time_stamp_month.max_by(&:length).length
-  File.stat(file.strip).mtime.month.to_s.rjust(file_time_stamp_month_length)
+def time_stamp_month(file)
+  time_stamp_m = @target_files.compact.map { |f| File.stat(f.strip).mtime.month.to_s }
+  time_stamp_m_length = time_stamp_m.max_by(&:length).length
+  File.stat(file.strip).mtime.month.to_s.rjust(time_stamp_m_length)
 end
 
-def file_time_stamp_day(file)
-  file_time_stamp_day = @target_files.compact.map { |f| File.stat(f.strip).mtime.day.to_s }
-  file_time_stamp_day_length = file_time_stamp_day.max_by(&:length).length
-  File.stat(file.strip).mtime.day.to_s.rjust(file_time_stamp_day_length)
+def time_stamp_day(file)
+  time_stamp_d = @target_files.compact.map { |f| File.stat(f.strip).mtime.day.to_s }
+  time_stamp_d_length = time_stamp_d.max_by(&:length).length
+  File.stat(file.strip).mtime.day.to_s.rjust(time_stamp_d_length)
 end
 
-def file_time_stamp_time(file)
-  file_time_stamp_time = @target_files.compact.map { |f| File.stat(f.strip).mtime.strftime('%H:%M') }
-  file_time_stamp_time_length = file_time_stamp_time.max_by(&:length).length
-  File.stat(file.strip).mtime.strftime('%H:%M').rjust(file_time_stamp_time_length)
+def time_stamp_time(file)
+  time_stamp_t = @target_files.compact.map { |f| File.stat(f.strip).mtime.strftime('%H:%M') }
+  time_stamp_t_length = time_stamp_t.max_by(&:length).length
+  File.stat(file.strip).mtime.strftime('%H:%M').rjust(time_stamp_t_length)
 end
 
-def file_owner(file)
+def owner_name(file)
   @target_files.compact.map { |f| Etc.getpwuid(File.stat(f.strip).uid).name }
   Etc.getpwuid(File.stat(file.strip).uid).name
 end
 
-def file_group(file)
+def group_name(file)
   @target_files.compact.map { |f| Etc.getgrgid(File.stat(f.strip).gid).name }
   Etc.getgrgid(File.stat(file.strip).gid).name
 end
@@ -94,11 +94,11 @@ end
 @target_files.reverse! if params['r']
 
 if params['l']
-  file_blocks = @target_files.compact.map { |file| File.stat(file.strip).blocks }
-  puts "total #{file_blocks.sum}"
+  block_size = @target_files.compact.map { |file| File.stat(file.strip).blocks }
+  puts "total #{block_size.sum}"
   @target_files.compact.each do |file|
-    puts "#{file_type(file)}#{file_mode(file)} #{file_hard_link(file)} #{file_owner(file)} #{file_group(file)} #{file_byte(file)}"\
-    " #{file_time_stamp_month(file)} #{file_time_stamp_day(file)} #{file_time_stamp_time(file)} #{file}"
+    puts "#{symbol_type(file)}#{permissions(file)} #{hard_link(file)} #{owner_name(file)} #{group_name(file)} #{byte_size(file)}"\
+    " #{time_stamp_month(file)} #{time_stamp_day(file)} #{time_stamp_time(file)} #{file}"
   end
 else
   ls_command(@target_files)
